a0f8403372421e91eb87a56323a22ea6bcecb903
ALBA-Girly <Albaumeh@gmail.com>
2025-11-22 20:47:37 +0100

    Add files via upload

---
+++ psycho.py
@@ -0,0 +1,9 @@
+
+age = int(input("Enter your age: "))
+
+if age < 10:
+    print("No, the age is less than 10.")
+elif age > 20:
+    print("No, the age is greater than 20.")
+else:
+    print("Yes, the age is between 10 and 20.")
